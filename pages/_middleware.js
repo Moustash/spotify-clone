0afdfd2c6aa8c87fa8aed8babda8c51a2488cecb
Corentin Chateau <corentin.chateau@advens.fr>
2021-12-17 18:41:11 +0100

    test middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,12 +5,18 @@ export async function middleware(req) {
   const token = await getToken({req, secret: process.env.JWT_SECRET})
   const {pathname } = req.nextUrl
 
-  if(pathname.includes('/api/auth') || token){
-    return NextResponse.next();
+  if(token && pathname === '/login'){
+    return NextResponse.redirect('/');
   }
 
-  if(!token && pathname !=='/login'){
-    return NextResponse.redirect('/login')
+  //if user wants to sign in
+  if(pathname.includes('/api/auth') || token){
+      return NextResponse.next();
+  }
+
+  //redirect to login if there is no token, and are requesting a protected route
+  if(!token && pathname !== '/login'){
+      return NextResponse.redirect('/login');
   }
 
 }
